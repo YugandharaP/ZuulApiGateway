06e101fd1f148e4618236129c5978a6b9f5aa619
Chaitra111 <111chaitra@gmail>
2018-08-16 19:41:32 +0530

    ZuulApiGateway commited

--- src/main/java/com/bridgelabz/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/bridgelabz/zuulserver/ZuulServerApplication.java
@@ -2,11 +2,11 @@ package com.bridgelabz.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulServerApplication {
 
